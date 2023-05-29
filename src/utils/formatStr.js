7a56fbb923cd03e9f7893ee920c92a33fd77d3a1
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2023-05-29 20:05:22 +0300

    refactoring apis

--- src/utils/formatStr.js
+++ src/utils/formatStr.js
@@ -2,7 +2,7 @@ const arabicNumbers  = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, 
 const englishNumbers  = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const formatStr = (str) => {
-    str = str.replace('ي','ى').replace('ة','ه')
+    // str = str.replace('ي','ى').replace('ة','ه')
     for(var i=0; i<10; i++)
     {
       str = str.replace(arabicNumbers[i], englishNumbers[i]);
